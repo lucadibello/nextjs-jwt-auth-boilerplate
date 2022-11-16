a44e694a638c6477300ae993087b26605f4a6221
Luca Di Bello <info@lucadibello.ch>
2022-11-16 19:42:59 +0100

    Added type import

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,8 +1,10 @@
+import { Middleware } from '../lib/types/middleware'
+
 export const withMiddlewares = (...middlewares: Middleware[]): Middleware => {
   // Create chain of middlewares
   const chain = middlewares.reduceRight(
     (next, middleware) => (req, res) => middleware(req, res, next),
-    (req, res) => res.status(200).json({ success: true })
+    (_req, res) => res.status(200).json({ success: true })
   )
 
   // Return chain of middlewares

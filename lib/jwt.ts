c711016763f3ebdbcc156424aa696c19254ecb71
Luca Di Bello <info@lucadibello.ch>
2022-11-16 18:19:02 +0100

    Style fixes

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -2,8 +2,7 @@
  * This library is used to generate confirmation tokens needed for certain actions.
  */
 
-import { verify } from "jsonwebtoken";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from 'jsonwebtoken'
 
 export const generateToken = <T extends Object | string>(
   payload: T,
@@ -12,9 +11,9 @@ export const generateToken = <T extends Object | string>(
 ) => {
   return sign(payload, secret, {
     expiresIn,
-  });
-};
+  })
+}
 
 export const verifyToken = (token: string, secret: string) => {
-  return verify(token, secret);
-};
+  return verify(token, secret)
+}

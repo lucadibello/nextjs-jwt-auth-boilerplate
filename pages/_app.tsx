8181aef4b2a5a92a5676e3e1981efeb5bd78e529
Luca Di Bello <info@lucadibello.ch>
2022-11-16 18:20:09 +0100

    Now using `AllProviders` provider wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import AllProviders from '../providers'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <AllProviders>
+        <Component {...pageProps} />
+      </AllProviders>
     </ChakraProvider>
-  );
+  )
 }

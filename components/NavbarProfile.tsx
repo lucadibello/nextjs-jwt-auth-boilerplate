8acee831662eb8d00ee6044d9aaf506f66bf22c7
Luca Di Bello <info@lucadibello.ch>
2022-11-16 18:18:42 +0100

    Added NavBar profile component

---
+++ components/NavbarProfile.tsx
@@ -0,0 +1,67 @@
+import {
+  Avatar,
+  Box,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  VStack,
+  Text,
+} from '@chakra-ui/react'
+import { UserSession } from '../lib/types/auth'
+import { FiChevronDown, FiLogOut } from 'react-icons/fi'
+
+interface INavbarProfileProps {
+  currentUser: UserSession
+  onLogOut: () => void
+}
+
+const NavbarProfile = ({ currentUser, onLogOut }: INavbarProfileProps) => {
+  return (
+    <Flex
+      alignItems={'center'}
+      gridColumnGap={{ base: '0', md: '6' }}
+      justifyContent="center"
+    >
+      <Box w="auto">
+        <Menu autoSelect={false}>
+          <MenuButton
+            py={2}
+            transition="all 0.3s"
+            _focus={{ boxShadow: 'none' }}
+          >
+            <Flex direction={'row'} alignItems="center">
+              <Avatar
+                size={'sm'}
+                src={'https://robohash.org/' + currentUser?.email}
+                name={currentUser.name + ' ' + currentUser.name}
+                bg={'white'}
+              />
+              <VStack
+                display={{ base: 'none', md: 'flex' }}
+                alignItems="flex-start"
+                spacing="1px"
+                ml="2"
+              >
+                <Text fontSize="sm" color="white">
+                  {currentUser.name} {currentUser.name}
+                </Text>
+              </VStack>
+              <Box display={{ base: 'none', md: 'flex' }}>
+                <FiChevronDown color="white" />
+              </Box>
+            </Flex>
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={onLogOut} icon={<FiLogOut />}>
+              Sign out
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Flex>
+  )
+}
+
+export default NavbarProfile

cc17d9e6552bd7e1f7c1f306208c8b4f122581dc
Luca Di Bello <info@lucadibello.ch>
2023-01-05 11:04:08 +0100

    HOTFIX: Removed debug mode from client

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,7 +10,6 @@ Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  debug: true,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
